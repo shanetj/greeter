4b3f59d598f8f8c9e77dde0b5eece7c013d07b8d
Shane Tjong <shanetj@gmail.com>
2011-09-07 13:19:59 -0400

    Testing

--- greeter.rb
+++ greeter.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/env ruby
 #This will greet people and stuff
 
+class Greeter
+ def hello name
+  puts "Hello, #{name}!"
+ end 
+end
+
+my_greeter = Greeter.new
+my_greeter.hello ARGV.first
+
+
